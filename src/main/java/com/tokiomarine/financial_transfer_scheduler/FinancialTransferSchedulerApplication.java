1cb9f94abefe062c34827c069f6f3cee889e7b69
Mariel Moura - Heimdall <mariel.moura@heimdallit.pt>
2025-05-21 15:33:32 -0300

    Initialize financial transfer scheduler project with Spring Boot setup

---
+++ src/main/java/com/tokiomarine/financial_transfer_scheduler/FinancialTransferSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.tokiomarine.financial_transfer_scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialTransferSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialTransferSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tokiomarine/financial_transfer_scheduler/FinancialTransferSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tokiomarine.financial_transfer_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialTransferSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
